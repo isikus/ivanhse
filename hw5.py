3c46bb92a99c97dbb8856dad75a96ba9c394c32a
isikus <ivantor@yandex.ru>
2016-12-06 01:15:32 +0400

    Ensured correct version of progrmming homework #5

--- hw5.py
+++ hw5.py
@@ -2,10 +2,10 @@ a={}
 total=0
 with5=0
 with open('intext.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
+	lines = f.readlines()
+	for line in lines:
 		total=total+1
-        a = line.split(' ')
-        if len(a)>4:
+		a = line.split(' ')
+		if len(a)>4:
 			with5=with5+1
 print(with5/total*100)
